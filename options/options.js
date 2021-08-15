18425f0150ea2ecf1fb44fce73394fa543f023cd
Crowdbotics <team@crowdbotics.com>
2021-08-15 17:10:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "spring_thunder_29826Identifier",
+  url: "https://spring_thunder_29826Identifier.botics.co",
+  api: "https://spring_thunder_29826Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
